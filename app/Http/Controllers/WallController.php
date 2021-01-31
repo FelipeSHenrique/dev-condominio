465559f1deabd148588bf3b5cbdde855a3e965d1
Felipe Henrique <49046577+FelipeSHenrique@users.noreply.github.com>
2021-01-31 16:58:57 -0300

    Adicionado sistema de login e Mural de Avisos

--- app/Http/Controllers/WallController.php
+++ app/Http/Controllers/WallController.php
@@ -4,7 +4,38 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\User;
+use App\Models\Wall;
+use App\Models\WallLike;
+
 class WallController extends Controller
 {
-    //
+    public function getAll() {
+        // Caso não tenha nada na lista eu retorno vazio, mas caso eu tenha alguns itens eu retorno na lista
+        $array = ['error' => '', 'list' => []];
+
+        $user = auth()->user();
+
+        $walls = Wall::all();
+
+        foreach($walls as $wallKey => $wallValue) {
+            $walls[$wallKey]['likes'] = 0;
+            $walls[$wallKey]['liked'] = false;
+
+            $likes = Walllike::where('id_wall', $wallValue['id'])->count();
+            $walls[$wallKey]['likes'] = $likes;
+
+            $meLikes = WallLike::where('id_wall', $wallValue['id'])
+            ->where('id_user', $user['id'])
+            ->count();
+
+            if ($meLikes > 0) {
+                $walls[$wallKey]['liked'] = true;
+            }
+        }
+
+        $array['list'] = $walls;
+
+        return $array;
+    }
 }
