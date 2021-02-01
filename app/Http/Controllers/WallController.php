5f292e25acbcd21461e136b0732ba97b801c284a
Felipe Henrique <49046577+FelipeSHenrique@users.noreply.github.com>
2021-02-01 20:47:48 -0300

    Adicionado sistema de like nos murais

--- app/Http/Controllers/WallController.php
+++ app/Http/Controllers/WallController.php
@@ -38,4 +38,35 @@ class WallController extends Controller
 
         return $array;
     }
+
+    public function like($id) {
+        $array = ['error' => ''];
+
+        $user = auth()->user();
+
+        $meLikes = WallLike::where('id_wall', $id)
+        ->where('id_user', $user['id'])
+        ->count();
+
+        if ($meLikes > 0) {
+            // Remover o like
+            WallLike::where('id_wall', $id)
+            ->where('id_user', $user['id'])
+            ->delete();
+
+            $array['liked'] = false;
+        } else {
+            // Adicionar o Like
+            $newLike = new WallLike();
+            $newLike->id_wall = $id;
+            $newLike->id_user = $user['id'];
+            $newLike->save();
+            $array['liked'] = true;
+        }
+
+        // Vou mostrar quantos likes eu tenho no total nessa postagem
+        $array['likes'] = WallLike::where('id_wall', $id)->count();
+
+        return $array;
+    }
 }
